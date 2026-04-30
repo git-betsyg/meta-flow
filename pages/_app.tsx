630b90aa583fea94c754c5287c2df0fe81fce18b
qwe <qwe@123.com>
2026-04-30 17:48:44 +0800

    feat(data): integrate react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,31 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
 import WebVitals from "@/components/web-vitals";
 import { SessionProvider } from "next-auth/react";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import React from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <>
       <WebVitals />
-      <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <SessionProvider session={session}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </SessionProvider>
+        </HydrationBoundary>
+      </QueryClientProvider>
     </>
   );
 }
